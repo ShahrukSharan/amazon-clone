0a9bbb22842bcbe6e8d5f6e6f8a5ac81145fdc50
ShahrukSharan <shahruksharan1590@gmail.com>
2021-06-21 19:14:33 +0600

    environment variables fixed

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_ZgABPEakwGMtuWHLOiEzPo1OwepYbVo",
+  apiKey: `${process.env.APP_KEY}`,
   authDomain: "clone-f65b7.firebaseapp.com",
   projectId: "clone-f65b7",
   storageBucket: "clone-f65b7.appspot.com",
   messagingSenderId: "650315213495",
-  appId: "1:650315213495:web:d749faf834a5ce6010bb32",
+  appId: `${process.env.APP_ID}`,
   measurementId: "G-6561JBMKM8",
 };
 
